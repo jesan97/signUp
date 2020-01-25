8c1342115e1292872ce9d88574191102394d193c
Macbook <ms@mss-MacBook-Pro.local>
2020-01-25 16:44:52 +0600

    this is sign up project

---
+++ Pods/Target Support Files/Pods-loginsingup/Pods-loginsingup-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_loginsingup : NSObject
+@end
+@implementation PodsDummy_Pods_loginsingup
+@end
---
+++ loginsingup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  loginsingup
+//
+//  Created by Macbook on 25/01/2020.
+//  Copyright © 2020 Macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
